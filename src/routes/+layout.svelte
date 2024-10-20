e0645f5a1ca835fdc4d9d99683fcf9c42c3827cb
Matthew Schwencke <matt.schwencke@proton.me>
2024-10-20 23:04:43 +0900

    Added basic pages and stying

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+	import '$lib/styles/pico.orange.min.css';
+	let { children } = $props();
+</script>
+
+<header>I'm a header</header>
+
+<main>
+	{@render children()}
+</main>
+
+<footer>I'm a footer</footer>
---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,5 @@
+<h1>Get in touch!</h1>
+
+<p>
+	<a href="https://aus.social/@Schwenckenator">Find me on Mastodon!</a>
+</p>
